69140090a2ae4bd23dc62dd9ed16a91683e8e6b4
vjskibin <vjskibin@gmail.com>
2018-06-21 16:22:15 +0300

    regular expression part2

--- src/lab1/RegularChecker.java
+++ src/lab1/RegularChecker.java
@@ -13,5 +13,53 @@ on 28.05.2018 | 3:39
 
 */
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegularChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку");
+        String string = scanner.nextLine();
+        System.out.println(check(string));
+    }
+
+    private static String check(String string)
+    {
+        String temp = string;
+        Pattern date = Pattern.compile("[\\d]{2}.[\\d]{2}.[\\d]{4}");
+        Pattern dateTime = Pattern.compile("[\\d]{2}.[\\d]{2}.[\\d]{4}T[\\d]{2}:[\\d]{2}");
+        Pattern email = Pattern.compile("^([A-Za-z0-9_-]+\\.)*[A-Za-z0-9_-]+@[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*\\.[A-Za-z]{2,6}$");
+
+        Matcher dateM = date.matcher(string);
+        Matcher dateTimeM = dateTime.matcher(string);
+        Matcher emailM = email.matcher(string);
+        if (dateTimeM.find())
+        {
+            temp = dateTimeM.replaceFirst("");
+            if (temp.length() != 0)
+                return "Nothing, just a string";
+            else
+                return "dateAndTime";
+        }
+        else if (dateM.find())
+        {
+            temp = dateM.replaceFirst("");
+            if (temp.length() != 0)
+                return "Nothing, just a string";
+            else
+                return "date";
+        }
+        else if (emailM.find())
+        {
+            temp = emailM.replaceFirst("");
+            if (temp.length() != 0)
+                return "Nothing, just a string";
+            else
+                return "email";
+        }
+        else
+            return "Nothing, just a string";
+    }
 }
